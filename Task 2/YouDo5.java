244620523114da6e5009478dc6778acd5b597d6e
Abid Shahriar <abid.shahriar.23@gmail.com>
2026-06-09 11:05:34 +0600

    Add Printer class with overloaded printData methods

--- Task 2/YouDo5.java
+++ Task 2/YouDo5.java
@@ -1,1 +1,30 @@
 
+class Printer {
+    
+    void printData(String text) {
+        System.out.println("String data: " + text);
+    }
+
+    
+    void printData(int number) {
+        System.out.println("Integer data: " + number);
+    }
+}
+
+
+
+
+
+
+public class Main5 {
+
+    public static void main(String[] args) {
+        Printer myPrinter = new Printer();
+
+        myPrinter.printData("Hello Java");
+
+        myPrinter.printData(101);
+
+    }
+    
+}
