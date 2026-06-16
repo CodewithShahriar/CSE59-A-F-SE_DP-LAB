276b67d90a630d04483fa12afa9d40bb55d52b2d
Abid Shahriar <abid.shahriar.23@gmail.com>
2026-06-16 09:57:50 +0600

    Add User, EmailValidator, and UserRepository classes

--- Task 3/YouDo.java
+++ Task 3/YouDo.java
@@ -1,1 +1,43 @@
+public class User {
+    private String username;
+    private String email;
 
+    public User(String username, String email) {
+        this.username = username;
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
+
+
+class EmailValidator {
+
+    public static boolean isValid(String email) {
+        if (email != null && email.contains("@")) {
+            return true;
+        }
+
+        System.out.println("Invalid email format.");
+        return false;
+    }
+}
+
+
+class UserRepository {
+
+    public void save (User user) {
+        if (EmailValidator.isValid(user.getEmail())) {
+            System.out.println("Connection to database...");
+            System.out.println(
+                "Saving user " + user.getUsername() + " to the user table ."
+            );
+        }
+    }
+}
